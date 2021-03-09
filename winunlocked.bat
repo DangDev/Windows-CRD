902aa2d5b4d869ffe16c254552670ba362d464ed
DangDev <54974645+DangDev@users.noreply.github.com>
2021-03-09 21:22:20 +0700

    Update winunlocked.bat

--- winunlocked.bat
+++ winunlocked.bat
@@ -36,3 +36,4 @@ cd Software
 
 certutil.exe -urlcache -split -f https://az764295.vo.msecnd.net/stable/622cb03f7e070a9670c94bae1a45d78d7181fbd4/VSCodeUserSetup-x64-1.53.2.exe
 certutil.exe -urlcache -split -f https://cdn-fastly.obsproject.com/downloads/OBS-Studio-26.1.1-Full-Installer-x64.exe
+certutil.exe -urlcache -split -f https://raw.githubusercontent.com/DangDev/Windows-CRD/main/EVKey64.exe
